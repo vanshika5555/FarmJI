b6b082b0b34a1b0b9a64fc448a01cd3ddaf2a0b9
Bhoomi Arora <168477294+NavyaArora24@users.noreply.github.com>
2025-01-18 22:30:36 +0500

    Add files via upload

---
+++ Bug Bunnies homepage/homepage.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <link rel="stylesheet" href="homepage.css">
+  <link
+    href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css"
+    rel="stylesheet"
+/>
+
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'> 
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+
+  <script src="homepage.js"></script>    
+
+  <!--navbar-->
+  <header>
+
+    <input type="checkbox" name="" id="toggler">
+    <label for="toggler" class="fas fa-bars"></label>
+
+
+  <a href="#" class="logo"> FARMIZ<span>.</span></a>
+
+  <nav class="navbar">
+    <a href="#">home</a>
+    <a href="#courses">Courses</a>
+    <a href="#about-us">About Us</a>
+    <a href="#our-vision">Our Vision</a>
+    <a href="#contact">Contact Us</a>
+    
+  </nav>
+
+  <div class="icons">
+    <a href="#"><i class="ri-account-circle-line"></i></a>
+    <a href="index.php"><i class="ri-shopping-cart-2-fill"></i></a>
+  </div>
+
+</header>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<section class="home">
+    <div class="home-text">
+        <img src = "img/welcomeImg.png" alt="">
+        <div class="box">
+    <h5>Let's, <br>
+    Grow together <br>
+    Let's Grow better</h5>
+    <p>We're introducing you with all advanced methods 
+      of growing <br> various fruits vegetables and crops. So
+      let's join hands together and grow better</p>
+      <div>
+  </div>
+</section>
+<br>
+<br>
+
+<!--Courses section-->
+
+<h1 class="heading-course"> Amazing Courses</h1>
+<section id="courses" class="courses">
+  <div class="container-course">
+  <div class="courses-box">
+    <img src="img/Avocado.jpeg" alt="course1">
+    <h2>Avocado Course</h2>
+    <p class="exp">-Learn how to grow Avocado</p>
+    <h4>4.7
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-regular fa-star-half-stroke"></i>
+      <i class="fa-regular fa-star"></i>
+      <br>
+      <div class="course-button">
+    <button class="btn">Explore Course</button>
+    <button class="btn">Meet Experts</button>
+</div>
+      </h4>
+  </div>
+
+
+  <div class="courses-box">
+    <img src="img/Fish Farming.jpeg" alt="course1">
+    <h2 class="coursename">Fish Farming Course</h2>
+    <p class="exp">-Learn how to do Fish Farming</p>
+    <h4>3.9
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-regular fa-star-half-stroke"></i>
+      <i class="fa-regular fa-star"></i>
+      <br>
+      <div class="course-button">
+    <button class="btn">Explore Course</button>
+    <button class="btn">Meet Experts</button>
+</div>
+      </h4>
+  </div>
+
+
+  <div class="courses-box">
+    <img src="img/Vermicomposting.jpeg" alt="course1">
+    <h2 class="coursename">Vermicomposting Course</h2>
+    <p class="exp">-Learn how to do Vermicomposting</p>
+    <h4>4.5
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-regular fa-star-half-stroke"></i>
+      <br>
+      <div class="course-button">
+    <button class="btn">Explore Course</button>
+    <button class="btn">Meet Experts</button>
+</div>
+    </h4>
+  </div>
+</div>
+
+<div class="container-course2">
+  <div class="courses-box">
+    <img src="img/chickenfarm.jpeg" alt="course1">
+    <h2 class="coursename">Chicken Farming Course</h2>
+    <p class="exp">-Learn how to do Chicken Farming</p>
+    <h4>4.0
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-regular fa-star"></i>
+      <br>
+      <div class="course-button">
+    <button class="btn">Explore Course</button>
+    <button class="btn">Meet Experts</button>
+</div>
+      </h4>
+  </div>
+
+  <div class="courses-box">
+    <img src="img/Asparagus.jpeg" alt="course1">
+    <h2 class="coursename">Asparagus</h2>
+    <p class="exp">-Learn how to grow Asparagus</p>
+    <h4>4.0
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-regular fa-star"></i>
+      <br>
+      <div class="course-button">
+    <button class="btn">Explore Course</button>
+    <button class="btn">Meet Experts</button>
+</div>
+      </h4>
+  </div>
+</div>
+</section>
+
+<section class="mentor">
+    <p>A Perfect Mentor,<br>
+A Perfect Solution</p>
+<img src="img/mentor.png" alt="">
+</section>
+
+<!--About Us-->
+
+<section class="about-us" id="about-us">
+      <div class="heading1">
+      <h1>ABOUT US</h1>
+      <p> WELCOME TO FARMIZ!! </p>
+      <p> At Farmiz, we are dedicated to nurturing the land and cultivating excellence.</p>
+      <p class="line1"> Where our passion for agriculture meets innovation sustainability. With a rich history in the heart of farming, we are dedicated to advancing the agricultural industry through cutting edge</p>
+      </div>
+      </section>
+
+      <!--Our Vision-->
+      <h1 class="vision-heading">OUR VISSION</h1>
+      <section class="our-vision" id="our-vision">
+      <div class="wrapper-vision">
+        <div class="card">
+            <img src="img/img2.jpg">
+            <div class="info">
+                <h1>Inovative Learning</h1>
+                <p>Provide accessible, interactive, and engaging courses, utilizing technology to reach farmers worldwide. </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="wrapper-vision">
+        <div class="card">
+            <img src="img/img.jpg">
+            <div class="info">
+                <h1>Empower Farmers</h1>
+                <p>Equip farmers with the knowledge and skills to grow new fruits and vegetables, increasing their yields and income. </p>
+               
+            </div>
+        </div>
+    </div>
+
+    <div class="wrapper-vision">
+        <div class="card">
+            <img src="img/img3.jpg">
+            <div class="info">
+                <h1>Recognition</h1>
+                <p>Celebrate and recognize farmers' achievements, innovations, and contributions to sustainable agriculture and food security.</p>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--Contact-us-->
+<section class="contact" id="contact">
+    <div class="container-contact">
+        <h2>CONTACT US</h2>
+        <div class="contact-wrapper">
+            <div class="contact-form">
+                <h3>Send us a message</h3>
+                <form action="connect.php" method="post">
+                    <div class="form-group">
+                        <input type="text" name="name" placeholder="Your Name" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" name="email" placeholder="Your Email Address" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="number" name="number" placeholder="Phone Number" required>
+                    </div>
+                    <div class="form-group">
+                        <textarea name="message" placeholder="Your Message" required></textarea>
+                    </div>                      
+                    <button type="submit">Send Message</button>  
+                </form>
+            </div>
+            
+        </div>
+    </div>
+
+<div class="wrapper"
+    <div class="title">
+    </div>
+    <div class="contact-form">
+        <div class="input-fields">
+           
+        </div>
+    </div>
+    </div>
+    </section>
+<!--footer-->
+<footer class="footer">
+    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <div class="footer-container">
+        <div class="footer-about">
+            <h2>About Farmiz</h2>
+            <p>Unlocking Farming Potential. Our courses teach farmers to cultivate new crops, boosting yields and income. Join our community and grow a better future!</p>
+        </div>
+        <div class="footer-links">
+            <h2>Quick Links</h2>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Our Courses</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+        <div class="footer-contact">
+            <h2>Contact Us</h2>
+            <ul>
+                <li>Email: support@farmiz.com</li>
+                <li>Phone: 91+ 9875656354</li>
+                <li>Address: C-123, Sector 22, Noida, Delhi - 201301</li>
+            </ul>
+        </div>
+        <div class="footer-social">
+            <h2>Follow Us</h2>
+            <ul>
+                <li>
+                    <a href="#" target="_blank">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" target="_blank">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" target="_blank">
+                        <i class="fa-brands fa-instagram"></i>
+                                            </a>
+                </li>
+                <li>
+                    <a href="#" target="_blank">
+                        <i class="fa-brands fa-linkedin-in"></i>
+                    </a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p>&copy; 2024 Farmiz. All rights reserved.</p>
+    </div>
+</footer>
+</body>
+</html>
